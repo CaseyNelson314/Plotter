93aafdc6c1611987cb1fccef6a666afaa47536c1
CaseyNelson314 <caseynelson314@gmail.com>
2022-11-04 00:31:09 +0900

    add commnt

--- Plotter/Plotter/Plotter.h
+++ Plotter/Plotter/Plotter.h
@@ -9,19 +9,22 @@ class Plotter {
 
 public:
 
-	Plotter(Font&& font = { 20 }, RectF&& parent = {}, const int resolution = 1000) noexcept
-		: points{}
-		, font(std::move(font))
+	/// @brief プロッタ作成
+	/// @param parent グラフを描画するエリア
+	/// @param resolution 分解能
 		, parent(std::move(parent))
 		, resolution(resolution ? resolution : 1)
 	{}
 
+	/// @brief サイズ更新
+	/// @param r 更新するサイズ
 	Plotter& resize(const RectF& r) {
 		parent = r;
 		return *this;
 	}
 
-	Plotter& put(double r) {
+	/// @brief プロットするデータをセット
+	/// @param r プロットするデータ
 		points.push_front(r);
 		if (points.size() > resolution) {
 			points.pop_back();
@@ -29,6 +32,8 @@ public:
 		return *this;
 	}
 
+	/// @brief グラフ描画
+	/// @param color グラフの色
 	Plotter& draw(const ColorF& color = Palette::White) {
 
 		const RectF plotArea = padding(10, 10, 30, 100);
@@ -37,7 +42,7 @@ public:
 
 		if (plotArea.h >= 0 && plotArea.w >= 0) {
 
-			/// @brief 最小最大値取得
+			/// 最小最大値取得
 			double max = std::numeric_limits<double>::min();
 			double min = std::numeric_limits<double>::max();
 			for (const auto& it : points) {
@@ -45,13 +50,13 @@ public:
 				min = std::min(it, min);
 			}
 
-			/// @brief 点の高さを求める
+			/// 点の高さを求める
 			const auto dataToPointHeight = [&](double data) ->double {
 				const double ratio = plotArea.h / (max - min) * (padding - 0.5) / padding/*margin top*/;  /// オートスケーリング用比率
 				return plotArea.right().end.y - (data - min) * ratio;
 			};
 
-			/// @brief グリッド描画
+			/// 点間の距離
 			for (auto i : step(padding + 1)) {
 				plotArea.top().movedBy(0, plotArea.h / padding * i).draw(Palette::Gray);
 			}
@@ -59,13 +64,13 @@ public:
 				plotArea.left().movedBy(plotArea.w / padding * i, 0).draw(Palette::Gray);
 			}
 
-			/// @brief 線分開始位置
+			/// 線分開始位置
 			Vec2 holdPoint = {
 				plotArea.right().end.x,
 				dataToPointHeight(points.front())
 			};
 
-			const double plotInterval = plotArea.w / (resolution - 1);
+			/// 全ての隣り合う点を線分で結ぶ
 
 			for (auto it = points.begin()++; it != points.end(); ++it) {
 				const Vec2 begin = holdPoint;
@@ -81,10 +86,13 @@ public:
 			plotArea.drawFrame(2, Palette::White);
 		}
 
-		parent.drawFrame(2, Palette::White);
+	/// @brief グリッド描画
+	/// @param horizontal 横方向の分割個数
+	/// @param vertical   縦方向の分割個数
 		return *this;
 	}
 
+	/// @brief グラフの表示域取得
 private:
 	RectF padding(double top, double right, double bottom, double left) {
 		return {
