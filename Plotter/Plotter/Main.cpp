96a5cfa5daf8abb4a2f70573b5b1d9ebbb8dc61c
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-11-04 16:29:46 +0900

    変数名変更

--- Plotter/Plotter/Main.cpp
+++ Plotter/Plotter/Main.cpp
@@ -4,21 +4,21 @@
 void Main() {
 	Window::SetStyle(WindowStyle::Sizable);
 
-	Plotter mainPlotter;
+	Plotter plotter1;
 	Plotter plotter2;
 
 	while (System::Update()) {
 		const auto plotterArea
 			= RectF{ Arg::center = Scene::Center(), Scene::Size() - Vec2{100, 100} };
 
-		mainPlotter
+		plotter1
 			.resize(plotterArea)
 			.plot(Periodic::Sawtooth0_1(5s))
 			.drawGrid(5, 4, Palette::Gray)
 			.draw(Palette::Blue);
 
 		plotter2
-			.resize(mainPlotter)
+			.resize(plotter1)
 			.plot(Periodic::Sine0_1(2s))
 			.draw(Palette::Red);
 	}
